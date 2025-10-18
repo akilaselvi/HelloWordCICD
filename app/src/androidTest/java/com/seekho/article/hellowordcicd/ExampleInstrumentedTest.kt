a5d9690d84b642c7a20feeee2c51bbe869297761
AkilPDev <developerakilap@gmail.com>
2025-10-18 10:22:22 +0530

    Intrument Test check

--- app/src/androidTest/java/com/seekho/article/hellowordcicd/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/seekho/article/hellowordcicd/ExampleInstrumentedTest.kt
@@ -1,5 +1,9 @@
 package com.seekho.article.hellowordcicd
 
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +11,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +20,21 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val composeRule = createAndroidComposeRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.seekho.article.hellowordcicd", appContext.packageName)
+    }
+
+    @Test
+    fun incrementTest(){
+        composeRule.onNodeWithText("Count: 0").assertExists()
+        composeRule.onNodeWithText("Submit").performClick()
+        composeRule.onNodeWithText("Count: 1").assertExists()
+
     }
 }
--- app/src/main/java/com/seekho/article/hellowordcicd/MainActivity.kt
+++ app/src/main/java/com/seekho/article/hellowordcicd/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import com.seekho.article.hellowordcicd.ui.theme.HelloWordCICDTheme
+import androidx.compose.runtime.*
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,11 +38,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String, modifier: Modifier = Modifier, viewModel: CountViewModel = viewModel()) {
+    val count by viewModel.count
+    Column {
+        Text(
+            text = "Count: $count",
+            modifier = modifier
+        )
+
+        Button(onClick = {
+            viewModel.increment()
+        }) {
+            Text(text = "Submit")
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
@@ -43,5 +61,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     HelloWordCICDTheme {
         Greeting("Android")
+    }
+}
+
+class CountViewModel : ViewModel(){
+    private val _count = mutableStateOf<Int>(0)
+    val count = _count
+
+    fun increment(){
+        _count.value++
     }
 }
